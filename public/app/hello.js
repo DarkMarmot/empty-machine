c64ca720f7740df510d95fd7f33b737911489db2
euf7315 <Daniel.Day@ParallonTechnology.com>
2018-01-30 13:35:09 -0600

    make 3d cube

--- public/app/hello.js
+++ public/app/hello.js
@@ -1,9 +1,55 @@
 Machine.cog({
 
-    display: `<div>hello bunny</div>`,
+    display: `<div id="webgl"></div>`,
 
     start() {
         console.log("HELLO!", THREE)
+
+        let scene = new THREE.Scene()
+
+        let box = this.getBox(1,1,1)
+
+        scene.add(box)
+
+        let camera = new THREE.PerspectiveCamera(
+            45,
+            window.innerWidth/window.innerHeight,
+            1,
+            1000
+        )
+
+        camera.position.x = 1
+
+        camera.position.y = 2
+
+        camera.position.z = 5
+
+        camera.lookAt(new THREE.Vector3(0,0,0))
+
+        let renderer = new THREE.WebGLRenderer()
+        renderer.setSize(window.innerWidth, window.innerHeight)
+
+        document.getElementById('webgl').appendChild(renderer.domElement)
+
+        renderer.render(
+            scene,
+            camera
+        )
+    },
+    getBox(x,y,z) {
+        let geometry = new THREE.BoxGeometry(1,1,1)
+
+        let material = new THREE.MeshBasicMaterial({
+            color: 0x00ff00
+        })
+
+        let mesh = new THREE.Mesh(
+            geometry,
+            material
+        )
+
+        return mesh
     }
+
 
 });
